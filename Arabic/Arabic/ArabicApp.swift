dcc2a57c5580783f7ed2b35c5d740666bf637ada
Azzam Alrashed <azzam.rar@gmail.com>
2026-05-26 17:43:01 +0300

    first commit

---
+++ Arabic/Arabic/ArabicApp.swift
@@ -0,0 +1,17 @@
+//
+//  ArabicApp.swift
+//  Arabic
+//
+//  Created by Azzam Alrashed on 26/05/2026.
+//
+
+import SwiftUI
+
+@main
+struct ArabicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
